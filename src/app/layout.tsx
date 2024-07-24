a5b9d402d2dc1181fc2f7d941e85a33da1fd085e
phạm bảo <phambao11062001@gmail.com>
2024-07-24 08:15:26 +0700

    change position header footer in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* <Header /> */}
-      <body className={inter.className}>{children}</body>
-      {/* <Footer /> */}
+
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import Footer from "../../components/footer/page";
-import Header from "../../components/header/page";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <main className={styles.main}>
+    <main className={styles.main}>
 
-      </main>
-      <Footer />
-    </>
+    </main>
   );
 }
