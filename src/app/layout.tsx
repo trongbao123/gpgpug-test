9cb865420015712c5d004271a1c04bc2756f36e0
Tien Nguyen <tien195164@huce.edu.vn>
2024-07-26 16:35:00 +0700

    change icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,28 +3,30 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../components/header/page";
 import Footer from "../components/footer/page";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "GPGPU",
-  description: "GPGPU Dashboard",
+    title: "GPGPU",
+    description: "GPGPU Dashboard",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-
-      <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
-      </body>
-
-    </html>
-  );
+    return (
+        <html lang="en">
+            <Head>
+                <link rel="shortcut icon" href="./logo.png" sizes="any" />
+            </Head>
+            <body className={inter.className}>
+                <Header />
+                {children}
+                <Footer />
+            </body>
+        </html>
+    );
 }
