52a11abfa1ea0f9b127cf2fd4fd4d5e8ce1faa78
Tien Nguyen <tien195164@huce.edu.vn>
2024-07-27 00:00:30 +0700

    add hover header main

--- src/app/home/_components/header-main/page.tsx
+++ src/app/home/_components/header-main/page.tsx
@@ -1,6 +1,9 @@
 import "./index.scss";
 
 const HeaderMain = () => {
+    const a = 2
+    console.log("🚀 ~ HeaderMain ~ a:", a)
+    
     return (
         <section className="header-main">
             <div className="bread">
