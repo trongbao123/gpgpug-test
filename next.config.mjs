89a3da37912318d21af9f1a63ca48e754eed8a44
Tien Nguyen <tien195164@huce.edu.vn>
2024-08-06 08:40:46 +0700

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
-import { join } from 'path';
-import Path from 'path';
+import { join } from "path";
+import Path from "path";
 const nextConfig = {
     sassOptions: {
-        includePaths: [
-            Path.join('styles')
-        ],
+        includePaths: [Path.join("styles")],
     },
-    images: { unoptimized: true },
+    images: { loader: "gpgpu-front", domain: "gpgpu-front.netlify.app" },
     swcMinify: true,
 };
 
