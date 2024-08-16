fd0b7b81075c8133c1a0ff4e42329df89e76edd9
phạm bảo <phambao11062001@gmail.com>
2024-08-16 09:04:18 +0700

    check button   verify code

--- src/components/auth-wrapper/authWrapper.tsx
+++ src/components/auth-wrapper/authWrapper.tsx
@@ -8,13 +8,16 @@ const AuthWrapper = (Component: any) => {
         const router = useRouter();
         const pathname = usePathname();
         const auth = localStorage.getItem(USERKIT_TOKEN);
-        const skipAuth = ["/auth/sign-in", "/auth/sign-in/email", "/auth/sign-up",];
+        const skipAuth = ["/auth/sign-in", "/auth/sign-in/email", "/auth/sign-up"];
 
         useEffect(() => {
+            if (auth) {
+                router.push(!skipAuth.includes(pathname) ? pathname : "/");
+            }
             if (!auth && !skipAuth.includes(pathname)) {
                 router.push("/auth/sign-in");
             }
-        }, []);
+        }, [auth, pathname]);
         if (!auth && !skipAuth.includes(pathname)) {
             return null;
         }
