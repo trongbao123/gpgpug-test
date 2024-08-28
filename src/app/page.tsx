63db0f73cc7db3e8284604f7a561fc5bab9d320e
phạm bảo <phambao11062001@gmail.com>
2024-08-28 17:18:09 +0700

    style modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
 "use client";
+import { useEffect, useState } from "react";
 import "../../styles/index.scss";
 import HomePage from "./home/page";
 import Project from "./project/page";
 
 const Home = () => {
-    const selected = localStorage.getItem("selectedProvider");
-    if (selected === "provider") {
-        return <HomePage />;
-    } else {
+    const [selected, setSelected] = useState<string | null>(null);
+
+    useEffect(() => {
+        const storedProvider = localStorage.getItem("selectedProvider");
+        setSelected(storedProvider);
+    }, []);
+
+    if (selected === "cloud") {
         return <Project />;
+    } else {
+        return <HomePage />;
     }
 };
 
